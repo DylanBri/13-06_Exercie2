2bb70859ac7b7fd594a65ca4afe43951b84004f4
Dylan Brito <dylan-brito@oultook.fr>
2022-06-10 15:31:55 +0200

    Ajoutez des fichiers projet.

---
+++ 13-06_Exercie2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _13_06_Exercie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fichier = File.ReadAllLines("..\\..\\..\\SongCSV.csv");
+            var donnees = fichier.Select(x => x.Split(",")).ToList();
+        }
+    }
+}
